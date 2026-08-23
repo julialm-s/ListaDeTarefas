e3b4c49d5cc44bf9d39b64852ce4fed596cc5e2a
julialm-s <julia.lima.713@ufrn.edu.br>
2026-08-23 18:18:27 -0300

    add arrays e renderizar tarefas

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function adicionarTarefa() {
+let tarefas = []
 
+function adicionarTarefa() {
       // recebe o valor do input do usuário
-      const inputTarefa = document.getElementById("inputTarefa")
+       const inputTarefa = document.getElementById("inputTarefa")
       let tarefa = inputTarefa.value.trim() //.trim() retira espaços em branco
       
       const mensagem = document.getElementById("mensagem") 
@@ -11,24 +12,30 @@ function adicionarTarefa() {
 
           let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
           mensagem.textContent = mensagemErro; 
-          mensagem.style.color = #A34743;
+          //mensagem.style.color = #A34743;
       
       } else {
         
           let mensagemSucesso = "Tarefa adicionada com sucesso!";
           let inputTarefa = document.getElementById("inputTarefa")
-
           let tarefa = inputTarefa.value
           mensagem.textContent = mensagemSucesso;
-
-          const listaTarefas = document.getElementById("listaTarefas")
-          let novaTarefa = document.createElement("li")
-   
-
-          novaTarefa.textContent = tarefa
-
-          listaTarefas.appendChild(novaTarefa)
+          tarefas.push(tarefa)
+          renderizartarefas()
       }
 
         inputTarefa.value = ""
+}
+
+function renderizartarefas() {
+      const listaTarefas = document.getElementById("listaTarefas")
+      listaTarefas.innerHTML = ""      
+
+      let i = 0 //iterador
+      for (i; i < tarefas.lenght; i++) {
+            let novaTarefa = document.createElement("li")
+            novaTarefa.textContent = tarefas[i]
+            listaTarefas.appendChild(novaTarefa)
+      }
+
 }
