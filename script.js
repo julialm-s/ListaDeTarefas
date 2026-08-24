39bd3ae13901302f6bf1ff74f903b769520d2aaf
julialm-s <julia.lima.713@ufrn.edu.br>
2026-08-23 21:59:22 -0300

    Add lógica remover, editar e limpar tudo

--- script.js
+++ script.js
@@ -21,21 +21,72 @@ function adicionarTarefa() {
           let tarefa = inputTarefa.value
           mensagem.textContent = mensagemSucesso;
           tarefas.push(tarefa)
-          renderizartarefas()
+          renderizarTarefas()
       }
 
         inputTarefa.value = ""
 }
 
-function renderizartarefas() {
+function renderizarTarefas() {
       const listaTarefas = document.getElementById("listaTarefas")
       listaTarefas.innerHTML = ""      
 
-      let i = 0 //iterador
-      for (i; i < tarefas.length; i++) {
+      // let i = 0 - iterador
+      for (let i = 0; i < tarefas.length; i++) {
             let novaTarefa = document.createElement("li")
+            
             novaTarefa.textContent = tarefas[i]
+
+
+            let botaoRemover = document.createElement("button") //criar botao para cada tarefa 
+            botaoRemover.className = "remover"
+            botaoRemover.textContent = "Remover Tarefa"
+            botaoRemover.onclick = () => removerTarefa(i) // function() {   removerTrefa()  } // Arrow function
+
+
+            let botaoEditar = document.createElement("button")
+            botaoEditar.className = "editar"
+            botaoEditar.textContent= "Editar Tarefa"
+            botaoEditar.onclick = () => editarTarefa(i)
+            
+            novaTarefa.appendChild(botaoRemover) // novaTarefa é o elemento "li"
+            novaTarefa.appendChild(botaoEditar)
             listaTarefas.appendChild(novaTarefa)
+            
+
+            function removerTarefa(i) {
+                  tarefas.splice(i, 1) // 1 - quantos itens deleta a partir do índice
+                  renderizarTarefas()
+                  const mensagem = document.getElementById("mensagem")
+                  mensagem.textContent = "Tarefa removida com sucesso!"
+                  
+            }
+      
+            function editarTarefa(i) {
+                  let tarefaEditada = prompt("Edite a tarefa:")
+                  if (tarefaEditada.trim !== "") {
+                      tarefas[i] = tarefaEditada
+                      renderizarTarefas()  
+                      const mensagem = document.getElementById("mensagem")
+                      mensagem.textContent = "Tarefa editada com sucesso!"
+                  }
+
+            }
       }
 
+if (tarefas.length>=1) {
+            let botaoLimparLista=document.createElement("button")
+            botaoLimparLista.className = "botao_lista"
+            botaoLimparLista.textContent = "Limpar Lista"
+            botaoLimparLista.onclick = () => limparLista()
+            listaTarefas.append(botaoLimparLista)
+
+} 
+
+function limparLista() {
+      tarefas.length = 0
+      renderizarTarefas()
+      const mensagem = document.getElementById("mensagem")
+      mensagem.textContent = "Lista de tarefas limpa com sucesso!"
+}
 }
